ba3cc40b0d65242bd6dd3eb3754d5c65ec01b0a4
Vitaliy Fomenko <vitaliy.fomenko@ab-soft.net>
2022-11-15 10:22:11 +0200

    HomeTask

--- script.js
+++ script.js
@@ -30,3 +30,7 @@ document.getElementById('loadItems').addEventListener('click', () => {
   xhttp.open('GET', '_items.html', true);
   xhttp.send();
 })
+
+document.getElementById('clearItems').addEventListener("click", ()=>{
+   document.getElementById('myList').innerHTML = "";
+})
